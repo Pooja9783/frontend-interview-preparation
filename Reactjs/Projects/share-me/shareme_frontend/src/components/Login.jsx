93a6f46ef8b4cc000126ffd1dfd375e38bf648e1
pooja sankhala <77524288+Pooja9783@users.noreply.github.com>
2022-11-30 13:09:48 +0530

    Added Navbar

--- Reactjs/Projects/share-me/shareme_frontend/src/components/Login.jsx
+++ Reactjs/Projects/share-me/shareme_frontend/src/components/Login.jsx
@@ -1,11 +1,30 @@
-import React from "react";
-import GoogleLogin from "react-google-login";
-import { FcGoogle } from "react-icons/fc";
+import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import { GoogleLogin, googleLogout } from "@react-oauth/google";
+import { FcGoogle } from "react-icons/fc";
 import shareVideo from "../assets/assets/share.mp4";
 import logo from "../assets/assets/logowhite.png";
+import { client } from "../client";
+import jwt_decode from "jwt-decode";
 
 const Login = () => {
+  const navigate = useNavigate();
+  const createOrGetUser = async (response) => {
+    const { name, picture, sub } = jwt_decode(response.credential);
+    // console.log(name, picture, sub);
+
+    const user = {
+      _id: sub,
+      _type: "user",
+      userName: name,
+      image: picture,
+    };
+    console.log(user);
+    client.createIfNotExists(user).then(() => {
+      navigate("/");
+    });
+    localStorage.setItem("user", JSON.stringify(user));
+  };
   return (
     <div className="flex justify-start-items-center flex-col h-screen">
       <div className="relative w-full h-full">
@@ -20,7 +39,15 @@ const Login = () => {
         />
         <div className="absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay">
           <div className="p-5">
-            <img scr={logo} width="130px" alt="logo" />
+            <img src={logo} width="130px" alt="logo" />
           </div>
           <div className="shadow-2xl">
-            <GoogleLogin clientId="" render={() => {}} />{"
+            <GoogleLogin onSuccess={createOrGetUser} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
