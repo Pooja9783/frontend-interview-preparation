cc16b9e98f82922d9a114c8980d02ed9a6445c2d
pooja sankhala <77524288+Pooja9783@users.noreply.github.com>
2022-12-07 22:26:00 +0530

    implemented counter with redux

---
+++ Reactjs/Redux/redux-app/src/redux/reducer.js
@@ -0,0 +1,22 @@
+import { INCREMENT, DECREMENT } from "./actionType";
+
+const initialState = {
+  counter: 0,
+};
+
+export const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case INCREMENT:
+      return {
+        ...state,
+        counter: state.counter + 1,
+      };
+    case DECREMENT:
+      return {
+        ...state,
+        counter: state.counter - 1,
+      };
+    default:
+      return state;
+  }
+};
---
+++ Reactjs/Redux/redux-app/src/redux/store.js
@@ -0,0 +1,5 @@
+import { legacy_createStore as createStore, applyMiddleware } from "redux";
+import { rootReducer } from "./rootReducer";
+import thunk from "redux-thunk";
+
+export const store = createStore(rootReducer);
