78905b99c05756ce730a11a25106a039205d5a79
pooja sankhala <77524288+Pooja9783@users.noreply.github.com>
2022-12-08 23:49:54 +0530

    Added rtk-query project

--- Javascript/javascript.js
+++ Javascript/javascript.js
@@ -1,2 +1,6 @@
-//What is javascript ?
-//Javascript is programming language that is used for making dynamic and inetractive web pages.
+//------------What is javascript?----------------
+//Javascript is the programming language.with the help of javascript we can make
+//a web page interactive and dynamic. Javascript can update and change both
+//HTML and CSS.
+
+//----------Javascript features------------------
